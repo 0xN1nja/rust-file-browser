2d54df818f7651b28fd6b364aad44c5132b7eacc
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-01-24 19:04:38 +0530

    Added file_io To Do Basic File Operations And Added get_files Function That Pushes Files In A Vector And Returns It

---
+++ src/file_io.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+use std::ffi::OsString;
+use std::fs::{*,self};
+use std::env::{*,self};
+use std::path::{*,self};
+
+pub fn get_files(_path:&Path)->Vec<OsString>{
+    let mut file_vec:Vec<OsString>=Vec::new();
+    if path::Path::exists(_path){
+        for i in fs::read_dir(_path).unwrap(){
+            file_vec.push(i.expect("").file_name());
+        }
+    }
+    else{
+        for i in fs::read_dir(env::current_dir().unwrap()).unwrap(){
+            file_vec.push(i.expect("").file_name());
+        }
+    }
+    file_vec
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![allow(unused)]
+
+mod file_io;
+use std::fs::{*,self};
+use std::env::{*,self};
+use std::path::{*,self};
+use file_io::{*};
+
 fn main(){
-    
+    let var=get_files(Path::new("C:\\"));
+    for i in var{
+        println!("{:?}",i.to_str().expect(""));
+    }
 }
