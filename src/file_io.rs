07eb4cf2fcc0ed1dda7b13a869b536b6475c32c8
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-01-24 19:15:51 +0530

    Formatted Code

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,26 +1,26 @@
 #![allow(unused)]
 
+use std::env::{self, *};
 use std::ffi::OsString;
-use std::fs::{*,self};
-use std::env::{*,self};
-use std::path::{*,self};
+use std::fs::{self, *};
+use std::path::{self, *};
 
-pub struct FileManager{
+pub struct FileManager {
     
 }
-impl FileManager{
-    pub fn get_files(_path:&Path)->Vec<OsString>{
-    let mut file_vec:Vec<OsString>=Vec::new();
-    if path::Path::exists(_path){
-        for i in fs::read_dir(_path).unwrap(){
-            file_vec.push(i.expect("").file_name());
+
+impl FileManager {
+    pub fn get_files(_path: &Path) -> Vec<OsString> {
+        let mut file_vec: Vec<OsString> = Vec::new();
+        if path::Path::exists(_path) {
+            for i in fs::read_dir(_path).unwrap() {
+                file_vec.push(i.expect("").file_name());
+            }
+        } else {
+            for i in fs::read_dir(env::current_dir().unwrap()).unwrap() {
+                file_vec.push(i.expect("").file_name());
+            }
         }
+        file_vec
     }
-    else{
-        for i in fs::read_dir(env::current_dir().unwrap()).unwrap(){
-            file_vec.push(i.expect("").file_name());
-        }
-    }
-    file_vec
-}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![allow(unused)]
 
 mod file_io;
-use std::fs::{*,self};
-use std::env::{*,self};
-use std::path::{*,self};
-use file_io::{*};
+use file_io::*;
+use std::env::{self, *};
+use std::fs::{self, *};
+use std::path::{self, *};
 
-fn main(){
-    let var=file_io::FileManager::get_files(Path::new("C:\\"));
-    for i in var{
-        println!("{:?}",i.to_str().expect(""));
+fn main() {
+    let var = file_io::FileManager::get_files(Path::new("C:\\"));
+    for i in var {
+        println!("{:?}", i.to_str().expect(""));
     }
 }
